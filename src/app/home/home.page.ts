e1fbedb31dbb359179c7ce4f6572b6f58691675d
Nazim Hickmet <toto@mail.com>
2019-03-29 10:55:41 +0100

    affichage des cartes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  private cardList: Array<{name: string, img: string, revealed: boolean}> = [];
+
+  private numberOfDistinctCards = 6;
+
+  private questionMarkUrl = '/assets/img/question-mark.png';
+
+  private generateDeck(){
+    for(let i =0; i < this.numberOfDistinctCards; i++){
+      let imgUrl = '/assets/img/cards/' + i + '.png';
+      this.cardList.push({img: this.questionMarkUrl, name: imgUrl, revealed: false });
+      this.cardList.push({img: this.questionMarkUrl, name: imgUrl, revealed: false });
+    }
+  }
+
+  constructor(){
+    this.generateDeck();
+    console.log(this.cardList);
+  }
+
+
+
 }
